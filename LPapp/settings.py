99dfe713105b282d96676c8f8cf94014941d84aa
Anshul Shah <anshulshah96@gmail.com>
2016-03-19 21:25:30 +0530

    minor UI tweaks

--- LPapp/settings.py
+++ LPapp/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import configuration
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
 
 
 
