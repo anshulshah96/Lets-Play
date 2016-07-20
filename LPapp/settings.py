c1161c2751123e3185df9a50f3f0f9f7db93c53b
Anshul Shah <anshulshah96@gmail.com>
2016-07-20 16:00:33 +0530

    Minor ui changes

--- LPapp/settings.py
+++ LPapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mc+w(=%disq6)#q5)6u2$y9l8ouyn5+$l7x)7v@gs8i3zh9%7#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = configuration.debug
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = configuration.allowed_hosts
 
 
