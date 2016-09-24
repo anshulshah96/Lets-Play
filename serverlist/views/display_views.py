961f42023748283b6d5bf50affea91a55c6f0605
Anshul Shah <anshulshah96@gmail.com>
2016-09-24 21:34:02 +0530

    Replaced old ui

--- serverlist/views/display_views.py
+++ serverlist/views/display_views.py
@@ -38,7 +38,6 @@ def index(request):
 				'leader_list_duration': leader_list_duration}
 	return render(request,'serverlist/index.html',context)
 
-@cache_page(20)
 def home(request):
 	return redirect("/serverlist/")
 
@@ -64,24 +63,3 @@ def handler500(request):
                                   context_instance=RequestContext(request))
     response.status_code = 500
     return response
-
-def index2(request):
-	latest_server_list = Server.objects.all()
-	logging.debug(latest_server_list)
-	for server in latest_server_list:
-		server.link = server.ip.replace(".","_")
-	leader_list_score = Player.objects.all().order_by("-score")[0:5]
-	leader_list_duration = Player.objects.all().order_by("-duration")[0:5]
-	context = {'latest_server_list': latest_server_list , 'leader_list_score': leader_list_score, 
-				'leader_list_duration': leader_list_duration}
-	return render(request,'serverlist2/index2.html',context)
-
-def ip_details2(request,server_id):
-	server_id = server_id.replace("_",".")
-	server = get_object_or_404(Server, ip = server_id)
-	human_list = PlayerTemp.objects.filter(server=server,bot=False).order_by("-score")
-	bot_list = PlayerTemp.objects.filter(server=server,bot=True).order_by("-score")
-	player_list = list(chain(human_list,bot_list))
-	logging.debug(player_list)
-	context = {'player_list': player_list,'server':server}
-	return render(request,'serverlist2/playerlist2.html',context)
